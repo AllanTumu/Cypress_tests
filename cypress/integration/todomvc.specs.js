dbaa3ec84f89fa143bbb18da2257b983fc8fad85
allan.tumuhimbise <allan.tumuhimbise@numbrs.com>
2022-04-02 00:32:03 +0300

    Added Validations or tests

--- cypress/integration/todomvc.specs.js
+++ cypress/integration/todomvc.specs.js
@@ -7,9 +7,20 @@ it('should add item to todo', () => {
     cy.visit('http://todomvc-app-for-testing.surge.sh/')
     //Enter task
     cy.get('.new-todo', {timeout: 6000}).type('Learn cypress{Enter}')
+    // Validate if element contains certain text
+    cy.get('label').should('have.text', 'Learn cypress')
+    // Verify that toogle is not checked
+    cy.get('.toggle').should('not.be.checked')
     //Mark task as done by clicking it
     cy.get('.toggle').click()
+    // Verify that toggle was checket
+    cy.get('.toggle').should('be.checked')
+    // Verify that text decoration is line through
+    cy.get('label').should('have.css', 'text-decoration-line','line-through')
     //Clear list by clicking Clear completed
     //We are using .contains and not .get
     cy.contains('Clear complete').click()
+    //Validate that todo list doesn't have any item
+    cy.get('.todo-list').should('not.have.descendants','li')
+    
 })
